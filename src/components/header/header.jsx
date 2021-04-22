11b8c5351fde11e227fae4a95e0e40a368fa905d
andreyscott <62455784+andreyscott@users.noreply.github.com>
2021-04-22 22:33:34 +0100

    created a fully functioning header components

---
+++ src/components/header/header.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+import { ReactComponent as Logo } from '../../assets/prestige.svg';
+
+import './header.scss';
+
+const Header = () => (
+    <div className='header'>
+        <Link className='logo-container' to='/'>
+       <Logo className='logo' />
+        </Link>
+        <div className='options'>
+            <Link className='options' to='/shop' >
+            Shop
+            </Link>
+            <Link className='options' to='/contact' >
+            Contact
+            </Link>
+            <Link className='options' to='/sigin' >
+            SignIn
+            </Link>
+        </div>
+    </div>
+)
+
+export default Header;
