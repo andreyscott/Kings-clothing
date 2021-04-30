d40202f8f79ddab1e4d74de2f8d5859feaeb38a9
andreyscott <62455784+andreyscott@users.noreply.github.com>
2021-04-30 22:48:36 +0100

    created a signin functionality

--- src/pages/sign-in_and_sign-up/sign-in_and_sign-up.jsx
+++ src/pages/sign-in_and_sign-up/sign-in_and_sign-up.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import SignIn from '../../components/signIn/SignIn';
+import SignUp from '../../components/sign-up/sign-up';
 
 import './sign-in.scss'
 
 const SignInAndSignUpPage = () => (
     <div className='sign-in-and-sign-up'>
         <SignIn />
+        <SignUp />
     </div>
 )
 
