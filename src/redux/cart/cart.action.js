b6a14d0b719bbd5ac5f9355a0d1a4d45cacc81c9
andreyscott <62455784+andreyscott@users.noreply.github.com>
2021-05-14 22:51:06 +0100

    finally fixed cart-deopdown error.Taking a Break from programmi

--- src/redux/cart/cart.action.js
+++ src/redux/cart/cart.action.js
@@ -4,7 +4,7 @@ export const toggleCartHidden = () => ({
     type: CartActionTypes.TOGGLE_CART_HIDDEN
 });
 
-export const addItem = (item) => ({
+export const addItem = item => ({
     type: CartActionTypes.ADD_ITEM,
-    payload: addItem
+    payload: item
 });
