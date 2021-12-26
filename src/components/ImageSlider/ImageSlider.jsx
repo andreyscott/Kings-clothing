3b4ef526167cb189ddd236f10a5e785d041d96c7
andreyscott <62455784+andreyscott@users.noreply.github.com>
2021-12-26 21:14:03 +0100

    I finished it

---
+++ src/components/ImageSlider/ImageSlider.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Slider from "react-slick";
+import { sliderImages, imageSliderConfig } from "../ImageSlider/imageSliderData";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./ImageSlider.css";
+const imageSlider = () => {
+  return (
+    <article>
+      <Slider className="imageSlider" {...imageSliderConfig}>
+        {sliderImages.map((image) => (
+          <div className="slider__container" key={image.id}>
+            <img className="slider__textImage" src={image.textImage} />
+            <div class="slider__imgContainer">
+              <img className="slider__img" src={image.url} />
+            </div>
+          </div>
+        ))}
+      </Slider>
+    </article>
+  );
+};
+
+export default imageSlider;
