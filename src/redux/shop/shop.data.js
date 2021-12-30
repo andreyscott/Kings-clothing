c47fe26d1a8f9761149fc200a3a69b5ec0e4bd3a
andreyscott <62455784+andreyscott@users.noreply.github.com>
2021-12-30 22:32:22 +0100

    modifying the shop page componennt

--- src/redux/shop/shop.data.js
+++ src/redux/shop/shop.data.js
@@ -201,69 +201,9 @@ const SHOP_DATA = {
       }
     ]
   },
-  hoddies: {
-    id: 99,
-    title: 'Hats',
-    routeName: 'hats',
-    items: [
-      {
-        id: 1,
-        name: 'Brown Brim',//https://i.ibb.co/ZYW3VTp/brown-brim.png
-        imageUrl: 'https://images.unsplash.com/photo-1618333845076-890b5baf8ffe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aG9vZGllc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
-        price: 25
-      },
-      {
-        id: 2,
-        name: 'Blue Beanie',
-        imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
-        price: 18
-      },
-      {
-        id: 3,
-        name: 'Brown Cowboy',
-        imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
-        price: 35
-      },
-      {
-        id: 4,
-        name: 'Grey Brim',
-        imageUrl: 'https://i.ibb.co/RjBLWxB/grey-brim.png',
-        price: 25
-      },
-      {
-        id: 5,
-        name: 'Green Beanie',
-        imageUrl: 'https://i.ibb.co/YTjW3vF/green-beanie.png',
-        price: 18
-      },
-      {
-        id: 6,
-        name: 'Palm Tree Cap',
-        imageUrl: 'https://i.ibb.co/rKBDvJX/palm-tree-cap.png',
-        price: 14
-      },
-      {
-        id: 7,
-        name: 'Red Beanie',
-        imageUrl: 'https://i.ibb.co/bLB646Z/red-beanie.png',
-        price: 18
-      },
-      {
-        id: 8,
-        name: 'Wolf Cap',
-        imageUrl: 'https://i.ibb.co/1f2nWMM/wolf-cap.png',
-        price: 14
-      },
-      {
-        id: 9,
-        name: 'Blue Snapback',
-        imageUrl: 'https://i.ibb.co/X2VJP2W/blue-snapback.png',
-        price: 16
-      }
-    ]
-  },
+  
   mens: {
-    id: 5,
+    id: 6,
     title: 'Mens',
     routeName: 'mens',
     items: [
@@ -302,6 +242,67 @@ const SHOP_DATA = {
         name: 'Burgundy T-shirt',
         imageUrl: 'https://i.ibb.co/mh3VM1f/polka-dot-shirt.png',
         price: 25
+      }
+    ]
+  },
+  hoddies: {
+    id: 5,
+    title: 'hoddies',
+    routeName: 'hoddies',
+    items: [
+      {
+        id: 36,
+        name: 'Brown Brim',//https://i.ibb.co/ZYW3VTp/brown-brim.png
+        imageUrl: 'https://images.unsplash.com/photo-1618333845076-890b5baf8ffe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aG9vZGllc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
+        price: 25
+      },
+      {
+        id: 37,
+        name: 'Blue Beanie',
+        imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
+        price: 18
+      },
+      {
+        id: 38,
+        name: 'Brown Cowboy',
+        imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
+        price: 35
+      },
+      {
+        id: 39,
+        name: 'Grey Brim',
+        imageUrl: 'https://i.ibb.co/RjBLWxB/grey-brim.png',
+        price: 25
+      },
+      {
+        id: 40,
+        name: 'Green Beanie',
+        imageUrl: 'https://i.ibb.co/YTjW3vF/green-beanie.png',
+        price: 18
+      },
+      {
+        id: 41,
+        name: 'Palm Tree Cap',
+        imageUrl: 'https://i.ibb.co/rKBDvJX/palm-tree-cap.png',
+        price: 14
+      },
+      {
+        id: 47,
+        name: 'Red Beanie',
+        imageUrl: 'https://i.ibb.co/bLB646Z/red-beanie.png',
+        price: 18
+      },
+      {
+        id: 48,
+        name: 'Wolf Cap',
+        imageUrl: 'https://i.ibb.co/1f2nWMM/wolf-cap.png',
+        price: 14
+      },
+      {
+        id: 49,
+        name: 'Blue Snapback',
+        imageUrl: 'https://i.ibb.co/X2VJP2W/blue-snapback.png',
+        price: 16
       }
     ]
   }
